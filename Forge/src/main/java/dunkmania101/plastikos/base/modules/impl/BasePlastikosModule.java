9b470385e608b7acc7a9b4eeb31c699d67ab63f9
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2023-04-18 13:20:22 -0400

    Refactor default requirement of `BASE` module.

--- Forge/src/main/java/dunkmania101/plastikos/base/modules/impl/BasePlastikosModule.java
+++ Forge/src/main/java/dunkmania101/plastikos/base/modules/impl/BasePlastikosModule.java
@@ -13,13 +13,20 @@ import dunkmania101.plastikos.base.modules.interfaces.IPlastikosModule;
 public abstract class BasePlastikosModule implements IPlastikosModule {
     protected final IPlastikosModule PARENT;
 
+    protected final boolean REQUIRE_BASE;
+
     public final Map<String, IPlastikosModule> MODULES;
     public final ArrayList<ModularModCreativeModeTab> CREATIVE_TABS;
 
-    public BasePlastikosModule(IPlastikosModule parent) {
+    public BasePlastikosModule(IPlastikosModule parent, boolean requireBase) {
         this.PARENT = parent;
+        this.REQUIRE_BASE = requireBase;
         this.MODULES = new HashMap<>();
         this.CREATIVE_TABS = new ArrayList<>();
+    }
+
+    public BasePlastikosModule(IPlastikosModule parent) {
+        this(parent, true);
     }
 
     @Override
@@ -40,10 +47,13 @@ public abstract class BasePlastikosModule implements IPlastikosModule {
     @Override
     public ArrayList<Pair<String, Boolean>> getDependencyIds() {
         ArrayList<Pair<String, Boolean>> deps = IPlastikosModule.super.getDependencyIds();
-        String baseId = PlastikosMod.INSTANCE.BASE.getId();
-        if (!getId().equals(baseId)) {
-            deps.add(Pair.of(baseId, true));
+        if (this.REQUIRE_BASE) {
+            String baseId = PlastikosMod.INSTANCE.BASE.getId();
+            if (!getId().equals(baseId)) {
+                deps.add(Pair.of(baseId, true));
+            }
         }
         return deps;
     }
 }
+
